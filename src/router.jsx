d1595ea64344f878269020a46f05a2778bd0128a
Willy Casana <willycasana@hotmail.com>
2022-10-14 11:45:49 -0500

    part3

--- src/router.jsx
+++ src/router.jsx
@@ -1,19 +1,14 @@
-
-
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-
+import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
-import ProductsPage from "./ProductsPage"
+import ProductsPage from "./ProductsPage";
 
-const myRouter= createBrowserRouter([{
-    element:<App/>,
-    path:"/"
-  },{
-    element:<ProductsPage/>,
-    path:"/productos"
-  }])
+export const myRouter = createBrowserRouter([
+  {
+    element: <App />,
+    path: "/"
+  },
+  {
+    element: <ProductsPage />,
+    path: "/productos"
+  }
+])
